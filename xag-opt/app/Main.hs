31f92c985133dfabc42ee74aa710b029790535ed
Joseph Lunderville <jlunderv@sfu.ca>
2024-11-06 20:06:49 -0800

    Add library dependencies

--- xag-opt/app/Main.hs
+++ xag-opt/app/Main.hs
@@ -1,5 +1,44 @@
 module Main where
 
+import Options.Applicative.Simple
+
+data Options = Options
+  { verbose :: Bool
+  -- ,
+  --   hello :: String,
+  --   enthusiasm :: Int
+  } deriving (Show)
+
+optionsParser :: Parser Options
+optionsParser =
+  Options
+    <$> switch
+      ( long "verbose"
+          <> short 'v'
+          <> help "Enable verbose logging"
+      )
+
+-- <*> strOption
+--   ( long "hello"
+--       <> metavar "TARGET"
+--       <> help "Target for the greeting"
+--   )
+-- <*> option
+--   auto
+--   ( long "enthusiasm"
+--       <> help "How enthusiastically to greet"
+--       <> showDefault
+--       <> value 1
+--       <> metavar "INT"
+--   )
+
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
+  (options, ()) <-
+    simpleOptions
+      "Version 0.1"
+      "xag-opt: optimize an XOR-AND graph"
+      "" -- description
+      optionsParser
+      empty
+  print $ "options: " ++ show options
